76910871fe52f6a4fb65f60dfa1de04859272495
adic2023 <adichatt25@gmail.com>
2026-05-17 23:37:46 -0400

    page

--- src/pages/Action.jsx
+++ src/pages/Action.jsx
@@ -1,1 +1,16 @@
-export default function Action() { return null }
+import Layout from '../Layout'
+import "../index.css"
+
+export default function Pledge() {
+  return (
+    <Layout>
+      <div className="masthead-wrap">
+        <h1 className="masthead">
+          Take Action Now!
+          <span className="masthead-sub"> Use the resources below to pressure Swarthmore College and DA Tanner Rause to Drop the Charges!</span>
+        </h1>
+        <div className="masthead-rule" />
+      </div>
+    </Layout>
+  )
+}
